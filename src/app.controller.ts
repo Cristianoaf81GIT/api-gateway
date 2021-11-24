f96a63e9fe8a445f25cf7e193f0038871a38f74b
Cristiano Alexandre de Faria <cristiano.alexandre@inatel.br>
2021-11-24 08:46:54 -0300

    aula(42): api gateway event emmiter

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,37 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {
+  Body,
+  Controller,
+  Get,
+  Logger,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import {
+  ClientProxy,
+  ClientProxyFactory,
+  Transport,
+} from '@nestjs/microservices';
+import { CriarCategoriaDto } from './dto/criar-categoria.dto';
 
-@Controller()
+@Controller('api/v1')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  private logger = new Logger(AppController.name);
+  private clientAdminBackend: ClientProxy;
+  constructor() {
+    this.clientAdminBackend = ClientProxyFactory.create({
+      transport: Transport.RMQ,
+      options: {
+        urls: ['amqp://user:uyH7hUdlwR7f@54.163.11.175:5672/smartranking'],
+        queue: 'admin-backend',
+      },
+    });
+  }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('categorias')
+  @UsePipes(ValidationPipe)
+  async criarCategoria(@Body() criarCategoriaDto: CriarCategoriaDto) {
+    this.logger.debug(`categoria recebida: ${criarCategoriaDto}`);
+    return this.clientAdminBackend.emit('criar-categoria', criarCategoriaDto);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { format } from 'date-fns-tz';
+import ptBR from 'date-fns/locale/pt-BR';
+import { AllExceptionsFilter } from './filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalFilters(new AllExceptionsFilter());
+  Date.prototype.toJSON = (): string =>
+    format(this, 'yyyy-MM-dd HH:mm:ss.SS', {
+      timeZone: 'America/Sao_Paulo',
+      locale: ptBR,
+    });
+  await app.listen(8080);
 }
 bootstrap();
