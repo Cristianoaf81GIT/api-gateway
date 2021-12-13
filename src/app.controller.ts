aef30b812e250a47ebb30d9b662f404c961e3b94
Cristiano Alexandre de Faria <cristiano.alexandre@inatel.br>
2021-12-13 07:36:50 -0300

    aula(47): adição método de atualização

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,13 +3,16 @@ import {
   Controller,
   Get,
   Logger,
+  Param,
   Post,
+  Put,
   Query,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import { ClientProxy, ClientProxyFactory } from '@nestjs/microservices';
 import { Observable } from 'rxjs';
+import { AtualizarCategoriaDto } from './dto/atualizar-categora.dto';
 import { CriarCategoriaDto } from './dto/criar-categoria.dto';
 
 @Controller('api/v1')
@@ -38,5 +41,17 @@ export class AppController {
   @Get('categorias')
   consultarCategorias(@Query('id') _id: string): Observable<any> {
     return this.clientAdminBackend.send('consultar-categorias', _id ? _id : '');
+  }
+
+  @Put('categorias/:_id')
+  @UsePipes(ValidationPipe)
+  atualizarCategoria(
+    @Body() atualizarCategoriaDto: AtualizarCategoriaDto,
+    @Param('_id') _id: string,
+  ) {
+    this.clientAdminBackend.emit('atualizar-categoria', {
+      id: _id,
+      categoria: atualizarCategoriaDto,
+    });
   }
 }
