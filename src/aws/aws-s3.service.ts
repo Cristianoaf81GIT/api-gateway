c0a7b09b77b8bd76f89cb47f7c4353b86d1dd747
Cristiano Alexandre de Faria <cristianoaf81@hotmail.com>
2023-01-24 18:41:20 -0300

    61. Refatorando o módulo AWS

--- src/aws/aws-s3.service.ts
+++ src/aws/aws-s3.service.ts
@@ -1,18 +1,18 @@
 import { BadGatewayException, Injectable, Logger } from '@nestjs/common';
 import * as aws from 'aws-sdk';
-import {AwsS3Config} from './aws-s3.config';
+import { AwsS3Config } from './aws-s3.config';
 
 @Injectable()
 export class AwsS3Service {
   private logger = new Logger(AwsS3Service.name);
 
-  constructor(private awsS3Config:AwsS3Config){}
+  constructor(private awsS3Config: AwsS3Config) {}
 
   public async uploadArquivo(file: any, id: string) {
     const s3 = new aws.S3({
       region: this.awsS3Config.AWS_REGION, //`${process.env.AWS_REGION}`,
       accessKeyId: this.awsS3Config.AWS_ACCESS_KEY_ID, //`${process.env.AWS_ACCESS_KEY_ID}`,
-      secretAccessKey:  this.awsS3Config.AWS_SECRET_ACCESS_KEY,//`${process.env.AWS_SECRET_ACCESS_KEY}`,
+      secretAccessKey: this.awsS3Config.AWS_SECRET_ACCESS_KEY, //`${process.env.AWS_SECRET_ACCESS_KEY}`,
     });
 
     const fileExtension = file.originalname.split('.')[1];
